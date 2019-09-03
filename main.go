7d21791f75d4d591a5b758f94022b7f1cdba5ea9
TatsuNet <yoichi.tatsuzumi@knocknote.co.jp>
2019-09-03 13:02:55 +0900

    Modify

--- main.go
+++ main.go
@@ -2,29 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/pkg/errors"
+	"time"
 
 	"go.knocknote.io/rapidash"
 )
 
 type UserLogin struct {
-	ID   int64
-	Name string
+	ID            int64
+	UserID        int64
+	UserSessionID int64
+	LoginParamID  int64
+	Name          string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
 
 func (u *UserLogin) EncodeRapidash(enc rapidash.Encoder) error {
 	if u.ID != 0 {
 		enc.Int64("id", u.ID)
 	}
+	enc.Int64("user_id", u.UserID)
+	enc.Int64("user_session_id", u.UserSessionID)
+	enc.Int64("login_param_id", u.LoginParamID)
 	enc.String("name", u.Name)
+	enc.Time("created_at", u.CreatedAt)
+	enc.Time("updated_at", u.UpdatedAt)
 	return enc.Error()
 }
 
 func (u *UserLogin) DecodeRapidash(dec rapidash.Decoder) error {
 	u.ID = dec.Int64("id")
+	u.UserID = dec.Int64("user_id")
+	u.UserSessionID = dec.Int64("user_session_id")
+	u.LoginParamID = dec.Int64("login_param_id")
 	u.Name = dec.String("name")
+	u.CreatedAt = dec.Time("created_at")
+	u.UpdatedAt = dec.Time("updated_at")
 	return dec.Error()
 }
 
@@ -52,7 +66,6 @@ func (u *UserLoginSlice) DecodeRapidash(dec rapidash.Decoder) error {
 	return nil
 }
 
-// Map column of `user_logins` table to Go type
 func (u *UserLogin) Struct() *rapidash.Struct {
 	return rapidash.NewStruct("user_logins").
 		FieldInt64("id").
@@ -65,8 +78,6 @@ func (u *UserLogin) Struct() *rapidash.Struct {
 }
 
 func main() {
-	fmt.Println("Start")
-
 	cache, err := rapidash.New(
 		rapidash.ServerAddrs([]string{"localhost:11211"}),
 		rapidash.Timeout(3*time.Second),
@@ -76,18 +87,29 @@ func main() {
 	}
 
 	tx, err := cache.Begin()
+	defer func() {
+		if err := tx.Commit(); err != nil {
+			fmt.Printf("err:%v\n", err)
+		}
+	}()
 	if err != nil {
 		panic(err)
 	}
-	defer func() {
-		if err := tx.Commit(); err != nil {
-			panic(err)
-		}
-	}()
+
+	if err := tx.Create("key", rapidash.Int(1)); err != nil {
+		panic(err)
+	}
+
+	var v int
+	if err := tx.Find("key", rapidash.IntPtr(&v)); err != nil {
+		panic(err)
+	}
+	fmt.Printf("v:%v\n", v)
 
 	userLogin := UserLogin{
-		ID:   1,
-		Name: "bob",
+		ID:     1,
+		UserID: 1,
+		Name:   "bob",
 	}
 
 	if err := tx.Create("user_login", new(UserLogin).Struct().Cast(&userLogin)); err != nil {
@@ -106,7 +128,7 @@ func main() {
 		panic(err)
 	}
 	var newUserLoginSlice UserLoginSlice
-	if err := tx.Find("user_login_slice",  rapidash.Structs(&newUserLoginSlice, new(UserLogin).Struct())); err != nil {
+	if err := tx.Find("user_login_slice", rapidash.Structs(&newUserLoginSlice, new(UserLogin).Struct())); err != nil {
 		panic(err)
 	}
 	fmt.Printf("newUserLoginSlice[0]:%v\n", newUserLoginSlice[0].Name)
